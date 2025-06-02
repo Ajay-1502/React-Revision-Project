26e64a81c3db0bfe67d5a5cb64dfe001820f8efa
Ajay-1502 <ahdesai40@gmail.com>
2025-06-02 21:11:11 +0530

    Controlled component for collecting user input

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import './AddUser.css';
 import Button from '../UI/Button';
 
 const AddUser = () => {
+  const [userName, setUserName] = useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(userName, age);
+    setUserName('');
+    setAge('');
   };
 
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={age}
+          onChange={(event) => setAge(event.target.value)}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
