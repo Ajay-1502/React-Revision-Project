b8ad0829f48c996f3990f070f84d7ff2037bd683
Ajay-1502 <ahdesai40@gmail.com>
2025-06-04 21:46:16 +0530

    Feature : Error modal to display the error message to the user

---
+++ src/Components/UI/ErrorModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ErrorModal.css';
+import Button from './Button';
+import Card from './Card';
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      <div className="backdrop" onClick={props.onConfirm}></div>
+      <Card className="modal">
+        <header className="header">
+          <h2>{props.title}</h2>
+        </header>
+        <div className="content">
+          <p>{props.message}</p>
+        </div>
+        <footer className="actions">
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </React.Fragment>
+  );
+};
+
+export default ErrorModal;
--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,42 +1,62 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import './AddUser.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
   const [userName, setUserName] = useState('');
   const [age, setAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (userName.length > 0 && age.length > 0) {
       age > 0 && props.onAddUser(userName, age);
+    } else {
+      setError({
+        title: 'Invalid Input',
+        message: 'Please enter a valid username & age',
+      });
+      return;
     }
-
     setUserName('');
     setAge('');
   };
 
+  const errorHandler = () => {
+    setError();
+  };
+
   return (
-    <Card className="input">
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={userName}
-          onChange={(event) => setUserName(event.target.value)}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          type="number"
-          value={age}
-          onChange={(event) => setAge(event.target.value)}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className="input">
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            value={age}
+            onChange={(event) => setAge(event.target.value)}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
